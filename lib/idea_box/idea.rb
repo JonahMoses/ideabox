12b3fedd3d99bcfdef4444cd89b7cb58c96c1daf
Jonah <Jonahkmoses@gmail.com>
2013-10-15 19:58:11 -0600

    everything up to extensions: tagging --you are able to view all ideas for a given tag

---
+++ lib/idea_box/idea.rb
@@ -0,0 +1,43 @@
+class Idea
+  include Comparable
+
+  attr_reader :title,
+              :description,
+              :rank,
+              :id,
+              :tags
+
+  def initialize(attributes = {})
+    @title       = attributes["title"]
+    @description = attributes["description"]
+    @rank        = attributes["rank"] || 0
+    @id          = attributes["id"]
+    @tags        = attributes["tags"]
+  end
+
+  def save
+    IdeaStore.create(to_h)
+  end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => rank,
+      "tags" => tags
+    }
+  end
+
+  def like!
+    @rank += 1
+  end
+
+  def tags
+    @tags
+  end
+
+  def <=>(other)
+    other.rank <=> rank
+  end
+
+end
